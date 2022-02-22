cd98c34e145d0a41a838420924f4d618a7217a85
Matt <mgstankov30@gmail.com>
2022-02-22 10:21:29 -0500

    add functionality to allow the game to be exited using the control c key options

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 const net = require('net');
+const { stdin, stdout } = require('process');
 const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
