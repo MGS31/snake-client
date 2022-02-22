892884ce5eb816ba434cacfb3569e9329216538f
Matt <mgstankov30@gmail.com>
2022-02-22 09:35:40 -0500

    moved code to client.js, add require client.js to utilize the client code in play.js

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
 const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-    conn.on('data', (data) => {
-    conn.end();
-  });
-    conn.on('end', () => {
-    console.log('you ded cuz you idled');
-  });
-
-
-  conn.setEncoding('utf8');
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
