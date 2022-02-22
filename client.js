a1e318cac0b1e35743e9352f035ceb2db7934ddc
Matt <mgstankov30@gmail.com>
2022-02-22 09:35:58 -0500

    add client.js folder to hold client code for snake project

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+    conn.on('data', (data) => {
+    conn.end();
+  });
+    conn.on('end', () => {
+    console.log('you ded cuz you idled');
+  });
+
+
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+module.exports = connect;
